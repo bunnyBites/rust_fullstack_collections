2fd7f41512c88b4830763bd09da05dea41cff009
bunnyBites <s.mukeshkr481@gmail.com>
2025-05-21 12:10:01 +0530

    add main async function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use dotenvy::dotenv;
+use reqwest::{Client, Method, header};
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    // create a shared reqwest client
+    let http_client = Client::new();
+
+    // Define CORS layer
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(vec![Method::GET, Method::POST, Method::OPTIONS])
+        .allow_headers(vec![header::CONTENT_TYPE, header::AUTHORIZATION]);
+
+    // Build application routes
+    let app = Router::new()
+        // .route("/api/chat", post(<post_method>))
+        .with_state(http_client)
+        .layer(cors);
+
+    // create app listening address
+    let addr = format!("0.0.0.0:{}", 3000); // can change 3000 to dynamic value
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
