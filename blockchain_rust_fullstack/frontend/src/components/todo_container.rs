fc0ea6eee2719fb50623e221b48b2d30ab7611cf
bunnyBites <s.mukeshkr481@gmail.com>
2025-06-14 12:27:58 +0530

    code optimization

--- blockchain_rust_fullstack/frontend/src/components/todo_container.rs
+++ blockchain_rust_fullstack/frontend/src/components/todo_container.rs
@@ -1,11 +1,10 @@
 use dioxus::{logger::tracing, prelude::*};
-use reqwest::Client;
 
 #[component]
 pub fn TodoContainer() -> Element {
     let mut user_public_key = use_signal(|| "".to_string());
 
-    tracing::info!("{user_public_key}");
+    tracing::info!("{:?}", user_public_key.read());
 
     rsx! {
       div { class: "container",
@@ -20,29 +19,35 @@ pub fn TodoContainer() -> Element {
                   placeholder: "Enter Solana Public Key",
                   value: "{user_public_key}",
                   oninput: move |event| user_public_key.set(event.value()), // Example: How you'd handle input
-              }
+              },
               // Button to trigger the fetch operation
               // You'll bind this to your fetch logic in the `onclick` handler
               button {
                   onclick: move |_| async move {
-                    if user_public_key.read().is_empty() {
-                      tracing::error!("No user public key provided");
-                    }
+                      let pub_key = user_public_key.read().clone();
 
-                    let get_url = format!("http://localhost:3000/sol/{}", user_public_key.read());
-
-                    match reqwest::get(get_url).await {
-                      Ok(response) => {
-                        tracing::info!("{:?}", response);
-                      },
-                      Err(err) => {
-                        tracing::error!("Issue: {}", err);
-                      }
-                    }
+                      on_ask_ai(&pub_key).await;
                   },
                   "Fetch To-Do"
               }
           }
       }
+    }
+}
+
+async fn on_ask_ai(provided_pub_key: &str) {
+    if provided_pub_key.is_empty() {
+        tracing::error!("No user public key provided");
+    }
+
+    let get_url = format!("http://localhost:3000/sol/{}", provided_pub_key);
+
+    match reqwest::get(get_url).await {
+        Ok(response) => {
+            tracing::info!("{:?}", response);
+        }
+        Err(err) => {
+            tracing::error!("Issue: {}", err);
+        }
     }
 }
